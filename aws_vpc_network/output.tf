783444b3ffd2c749f3b68fe936081992e75a6b43
dhutsj <shuangjie.tian@elsevier.com>
2023-01-03 11:27:50 +0800

    add output

---
+++ aws_vpc_network/output.tf
@@ -0,0 +1,15 @@
+output "private_subnet1_id" {
+  value = aws_subnet.private.id
+}
+
+output "private_subnet2_id" {
+  value = aws_subnet.private2.id
+}
+
+output "public_subnet1_id" {
+  value = aws_subnet.public.id
+}
+
+output "public_subnet2_id" {
+  value = aws_subnet.public2.id
+}
--- aws_vpc_network/subnet.tf
+++ aws_vpc_network/subnet.tf
@@ -16,3 +16,21 @@ resource "aws_subnet" "public" {
     Name = "myvpc_public"
   }
 }
+
+resource "aws_subnet" "private2" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "192.168.3.0/24"
+
+  tags = {
+    Name = "myvpc_private2"
+  }
+}
+
+resource "aws_subnet" "public2" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "192.168.4.0/24"
+
+  tags = {
+    Name = "myvpc_public2"
+  }
+}

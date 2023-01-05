13c0a4f16b92b5473677c26aeffc1535ea867513
Justin Tian <jszxtsj@163.com>
2023-01-05 14:11:13 +0800

    Update example.sh

--- aws_modern_app/example.sh
+++ aws_modern_app/example.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-sudo yum update -y
-sudo yum install nginx -y
-sudo systemctl enable nginx
-sudo systemctl start nginx
+
 sudo echo $(hostname) > /usr/share/nginx/html/index.html
+sudo systemctl start nginx
